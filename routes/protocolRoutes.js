a96273466c6f087c3eb1ee6d7b68bba2c1c5b1dd
Superior5 <superiorunknown5@gmail.com>
2023-11-06 23:11:15 +0300

    fix

--- routes/protocolRoutes.js
+++ routes/protocolRoutes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import fileMiddleware from '../middleware/file.js';
-import { addMedia, addProtocol, getProtocols, getTranscribe} from "../controllers/protocolController.js";
+import { addMedia, addProtocol, getProtocols,getProtocol , getTranscribe} from "../controllers/protocolController.js";
 
 const router = Router();
 
 router.post('/addMedia', fileMiddleware.single('file'), addMedia);
 router.post('/addProtocol', addProtocol);
 router.get('/getProtocols', getProtocols);
+router.get('/getProtocols', getProtocol);
 router.get('/getTranscribe', getTranscribe);
 
 export default router;
