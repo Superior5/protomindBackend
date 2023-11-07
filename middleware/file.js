c792abc168b1213a3fb5931e01d398adf003ace0
Superior5 <superiorunknown5@gmail.com>
2023-11-07 16:42:18 +0300

    fix

--- middleware/file.js
+++ middleware/file.js
@@ -1,7 +1,9 @@
 import multer from "multer";
 
 const storage = multer.diskStorage({
+    
     destination(req, file, cb){
+        console.log('hello')
         cb(null, 'uploads/videos/');
     },
     filename(req, file, cb) {
